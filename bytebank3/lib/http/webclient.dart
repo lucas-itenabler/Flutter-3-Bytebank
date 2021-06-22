a92c732db0d4427102a265b4d2906be5bdb44b78
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-22 13:03:43 -0300

    Adicionando interceptador

--- bytebank3/lib/http/webclient.dart
+++ bytebank3/lib/http/webclient.dart
@@ -1,12 +1,36 @@
-
 import 'package:http/http.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print('Request');
+    print(data.toHttpRequest());
+    print(' headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print('Response');
+    print(' status code: ${data.statusCode}');
+    print(' headers: ${data.headers}');
+    print('body: ${data.body}');
+    return data;
+  }
+}
 
 void findAll() async {
+  Client client = InterceptedClient.build(
+    interceptors: [
+      LoggingInterceptor(),
+    ],
+  );
   final url = Uri.http(
     '192.168.0.35:8080',
     'transactions',
   );
 
-  final response = await get(url);
-  print(response.body.toString());
+  final response = await client.get(url);
 }
