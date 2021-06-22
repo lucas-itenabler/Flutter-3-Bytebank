2023ea28bb7e76e7e3656d536ed8766a752cf8be
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-06-22 12:30:52 -0300

    Consertando erro do ultimo commit

--- bytebank3/lib/http/webclient.dart
+++ bytebank3/lib/http/webclient.dart
@@ -3,10 +3,10 @@ import 'package:http/http.dart';
 
 void findAll() async {
   final url = Uri.http(
-    'http://192.168.0.35/',
+    '192.168.0.35:8080',
     'transactions',
   );
 
   final response = await get(url);
-  print(response.body);
+  print(response.body.toString());
 }
